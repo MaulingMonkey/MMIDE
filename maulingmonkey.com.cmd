36df47ae94e6bcee7fdd2d67cd9c3b9b3736dc40
MaulingMonkey <nobody@nowhere.noplace>
2016-07-27 18:51:00 -0700

    Upload script

---
+++ maulingmonkey.com.cmd
@@ -0,0 +1,3 @@
+mkdir                           I:\home\website\maulingmonkey.com\brainfuck\
+robocopy /S   %~dp0\MMIDE       I:\home\website\maulingmonkey.com\brainfuck       *.png *.css *.html *.js
+pause
