1deb38b0ee15c5547b824c4c73ff77b6b0aadfc9
MaulingMonkey <nobody@nowhere.noplace>
2016-08-17 19:32:42 -0700

    Add deploy checklist to deploy script

--- maulingmonkey.com.cmd
+++ maulingmonkey.com.cmd
@@ -1,3 +1,13 @@
+@git status
+@echo.
+@echo     DEPLOY CHECKLIST:
+@echo [ ] Verify you committed everything
+@echo [ ] Check for log spam
+@echo [ ] Verify ancillary pages still work properly
+@echo [ ] Maybe build Release instead of Debug
+@echo.
+@pause
+
 mkdir                           I:\home\website\maulingmonkey.com\brainfuck\
 robocopy /S   %~dp0\MMIDE       I:\home\website\maulingmonkey.com\brainfuck       *.png *.css *.html *.js
 pause
